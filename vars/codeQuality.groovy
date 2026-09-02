09d3b8d158ead74a99243d4dd36beb22a7146f47
hillel <113309415+hshemasian@users.noreply.github.com>
2026-09-02 20:41:30 +0300

    Update codeQuality.groovy

--- vars/codeQuality.groovy
+++ vars/codeQuality.groovy
@@ -1,22 +1,12 @@
 def sonarCreateProject(String projectKey) {
         withSonarQubeEnv('SonarQubeScanner') {
-            sh """
-                curl -s -u ${env.SONAR_TOKEN}: \
-               -X POST "${env.SONAR_HOST_URL}/api/projects/create" \
-               -d "project=${projectKey}&name=${projectKey}"
-            """
+            sh 
         }
 }
 
 def sonarLocalScan() {
     def scannerHome = tool 'SonarQubeScanner'
     withSonarQubeEnv('SonarQubeScanner') {
-        sh """
-            ${scannerHome}/bin/sonar-scanner \
-            -Dsonar.projectKey=${env.JOB_NAME} \
-            -Dsonar.projectName=${env.JOB_NAME} \
-            -Dsonar.sources=. \
-            -Dsonar.sourceEncoding=UTF-8
-        """
+        sh
     }
 }
