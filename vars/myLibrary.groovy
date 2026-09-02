d6ca583e8383587e802ab9941370b6762e7f8cbc
hillel <113309415+hshemasian@users.noreply.github.com>
2026-09-02 19:04:18 +0300

    Update myLibrary.groovy

--- vars/myLibrary.groovy
+++ vars/myLibrary.groovy
@@ -1,1 +1,11 @@
+def deployApp(String branch = 'main') {
+    echo "Deploying application from branch: ${branch}"
+}
 
+def cleanup() {
+    echo "Running cleanup tasks"
+}
+
+def buildApp() {
+    echo "Building the application"
+}
