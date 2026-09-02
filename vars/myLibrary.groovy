4203eb664de31125a3914214b2211884408c224d
hillel <113309415+hshemasian@users.noreply.github.com>
2026-09-02 19:27:05 +0300

    Update myLibrary.groovy

--- vars/myLibrary.groovy
+++ vars/myLibrary.groovy
@@ -2,10 +2,24 @@ def deployApp(String branch = 'main') {
     echo "Deploying application from branch: ${branch}"
 }
 
-def cleanup() {
-    echo "Running cleanup tasks"
+def buildApp(String imageName) {
+    echo "Building Docker image: ${imageName}"
+    sh "docker build -t ${imageName} ."
 }
 
-def buildApp() {
-    echo "Building the application"
+def testApp() {
+    echo 'Running tests...'
+}
+
+def deployToDockerHub() {
+    echo "Deploying ${env.IMAGE_NAME} to Docker Hub..."
+                withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'USER', passwordVariable: 'PAT')]) {
+                    sh 'echo "$PAT" | docker login -u "$USER" --password-stdin'
+                    sh "docker push ${env.IMAGE_NAME}"
+                }
+}
+
+def cleanup() {
+    echo "Cleaning up local Docker image..."
+                sh "docker rmi ${env.IMAGE_NAME} || true"
 }
