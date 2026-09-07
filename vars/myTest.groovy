d17d741dec090d692e65c5c4adbe4895b568d233
hillel <113309415+hshemasian@users.noreply.github.com>
2026-09-07 21:15:12 +0300

    Update myTest.groovy

--- vars/myTest.groovy
+++ vars/myTest.groovy
@@ -1,1 +1,33 @@
+podTemplate(
+    label: 'my-multi-pod',
+    containers: [
+        containerTemplate(
+            name: 'ubuntu',
+            image: 'ubuntu:latest',
+            ttyEnabled: true,
+            command: 'cat'
+        ),
+        containerTemplate(
+            name: 'alpine',
+            image: 'alpine:latest',
+            ttyEnabled: true,
+            command: 'cat'
+        )
+    ]
+) {
+    node('my-multi-pod') {
 
+        stage('Hello Ubuntu') {
+            container('ubuntu') {
+                sh 'echo Hello World from Ubuntu'
+            }
+        }
+
+        stage('Hello Alpine') {
+            container('alpine') {
+                sh 'echo Hello World from Alpine'
+            }
+        }
+
+    }
+}
