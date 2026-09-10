a240640080e03f2765d4bebbc5190fac0b75491f
hillel <113309415+hshemasian@users.noreply.github.com>
2026-09-10 13:49:36 +0300

    Update securityScan.groovy

--- vars/securityScan.groovy
+++ vars/securityScan.groovy
@@ -1,10 +1,8 @@
 def call(Map config = [:]) {
     container('trivy') {
-        echo "--- סריקת אבטחת רכיבים ותלויות עם Trivy ---"
+        echo "--- סריקת אבטחה עם Trivy ---"
         sh 'trivy fs --format json --output trivy-android-report.json .'
         sh 'trivy fs --exit-code 0 --severity HIGH,CRITICAL .'
     }
-    
-    // שמירת הדו"ח מחוץ לקונטיינר בברמת ה-Pipeline
     archiveArtifacts artifacts: 'trivy-android-report.json', allowEmptyArchive: true
 }
