c2ab43aa408c92741ca2e3df9fff227b575e1049
hillel <113309415+hshemasian@users.noreply.github.com>
2026-09-10 13:50:04 +0300

    Update buildApk.groovy

--- vars/buildApk.groovy
+++ vars/buildApk.groovy
@@ -1,12 +1,10 @@
 def call(Map config = [:]) {
     container('android-builder') {
-        echo "--- קימפול האפליקציה ויצירת קובץ APK ---"
+        echo "--- קימפול ה-APK ---"
         dir('android') {
             sh 'chmod +x gradlew'
             sh './gradlew assembleDebug'
         }
     }
-    
-    // שמירת קובץ ה-APK
     archiveArtifacts artifacts: '**/build/outputs/apk/debug/*.apk', allowEmptyArchive: false
 }
